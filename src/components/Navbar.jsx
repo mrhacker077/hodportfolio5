596cb8f2cf741eaf69a1a35fa31bd927a0a65735
mrhacker077 <manjunathm7091@gmail.com>
2025-02-09 20:00:32 +0530

    neww

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from "react";
 import pic from "../at.jpg";
 import { AiOutlineMenu } from "react-icons/ai";
@@ -19,6 +20,10 @@ function Navbar() {
       text: "Skills",
     },
     {
+      id: 4,
+      text: "Experience",
+    },
+    {
       id: 5,
       text: "Contact",
     },
@@ -30,7 +35,7 @@ function Navbar() {
           <div className=" flex space-x-2">
             <img src={pic} className="h-12 w-12 rounded-full" alt="" />
             <h1 className="font-semibold text-xl cursor-pointer">
-              Devraju.D KPT Mangalore<span className="text-green-500 text-2xl"></span>
+            Devraju.D KPT Mangalore<span className="text-green-500 text-2xl"></span>
               <p className="text-sm">Lecturer</p>
             </h1>
           </div>
